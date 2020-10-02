e7cdded80dd63d7277fc95f5b9ff88922d7e1de6
Anderson Danilo <contact@andersondanilo.com>
2020-10-01 21:53:08 -0300

    adjustments (working)

--- src/bundles/app_bundle/controller/crud_controller.cr
+++ src/bundles/app_bundle/controller/crud_controller.cr
@@ -7,19 +7,12 @@ require "../repository/crud_repository"
 require "../service/crud_service"
 
 module App
-  abstract class CrudController(T) < Glassy::HTTP::Controller
-    # def initialize(
-    #   @serializer : Serializer(T),
-    #   @service : CrudService(T),
-    #   @repository : CrudRepository(T),
-    #   @policy : CrudPolicy(T),
-    #   @user_repository : UserRepository
-    # )
-    # end
-
-    def path_prefix : String
-      raise "Must set a path prefix for crud controller"
-    end
+  module CrudController
+    protected abstract def serializer : Serializer(T)
+    protected abstract def service : CrudService(T)
+    protected abstract def repository : CrudRepository(T)
+    protected abstract def policy : CrudPolicy(T)
+    protected abstract def user_repository : UserRepository
 
     @[Route("POST", "/", middlewares: ["auth"])]
     @[Context(arg: "ctx")]
@@ -32,17 +25,17 @@ module App
 
       user = get_user(ctx)
 
-      entity = @serializer.deserialize!(json_any.not_nil!)
+      entity = serializer.deserialize!(json_any.not_nil!)
 
-      unless @policy.can_create?(entity, user)
+      unless policy.can_create?(entity, user)
         raise Glassy::HTTP::Exceptions::UnauthorizedException.new("Permission denied")
       end
 
-      @service.create(entity)
+      service.create(entity)
 
       ctx.response.status_code = 201
 
-      @serializer.serialize(entity)
+      serializer.serialize(entity)
     end
 
     @[Route("GET", "/:id", middlewares: ["auth"])]
@@ -51,22 +44,22 @@ module App
       id = ctx.params.url["id"]
       user = get_user(ctx)
 
-      entity = @repository.find_by_id(id)
+      entity = repository.find_by_id(id)
 
       if entity.nil?
         raise Glassy::HTTP::Exceptions::NotFoundException.new("Resource not found")
       end
 
-      unless @policy.can_show?(entity, user)
+      unless policy.can_show?(entity, user)
         raise Glassy::HTTP::Exceptions::UnauthorizedException.new("Permission denied")
       end
 
-      @serializer.serialize(entity)
+      serializer.serialize(entity)
     end
 
     def get_user(ctx : HTTP::Server::Context) : User
       user_id = ctx.get("user_id").to_s
-      user = @user_repository.find_by_id(user_id)
+      user = user_repository.find_by_id(user_id)
 
       if user.nil?
         raise Glassy::HTTP::Exceptions::UnauthorizedException.new("User not found")
