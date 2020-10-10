969085f8070f9e12a78facc636d66a2c594fef62
Anderson Danilo <contact@andersondanilo.com>
2020-10-10 14:22:04 -0300

    i18n

--- src/bundles/app_bundle/validator/product_category_validator.cr
+++ src/bundles/app_bundle/validator/product_category_validator.cr
@@ -1,12 +1,18 @@
+require "glassy-i18n"
 require "../model/product_category"
 require "../exception/validation_exception"
 require "./validator"
 
 module App
   class ProductCategoryValidator < Validator(ProductCategory)
+    def initialize(@i18n : Glassy::I18n::Translator)
+    end
+
     def validate!(entity : ProductCategory) : Void
       if entity.name.nil? || entity.name == ""
-        raise ValidationException.new("Name is required")
+        raise ValidationException.new(@i18n.t("validation.messages.required", {
+          "attr" => @i18n.t("validation.attributes.name")
+        }))
       end
     end
   end
